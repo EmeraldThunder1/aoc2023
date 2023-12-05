6c92e3a5e6f7fa3a4ec3da206352b408e8b1f6b2
EmeraldThunder1 <git_hub@dapoole.co.uk>
2023-12-05 09:35:44 +0000

    Completed Day 5 (very inefficient)

---
+++ Day 5/code1.ts
@@ -0,0 +1,68 @@
+import { readFileSync } from "fs";
+
+interface SeedMap {
+    id: string;
+    ranges: Range[];
+}
+
+interface Range {
+    destinationStart: number;
+    sourceStart: number;
+    rangeLength: number;
+}
+
+function getMapValue(ranges: Range[], key: number): number {
+    let result = key;
+
+    for (const range of ranges) {
+        if (key >= range.sourceStart && key < range.sourceStart + range.rangeLength) {
+            result = range.destinationStart + (key - range.sourceStart);
+            break;
+        }
+    }
+
+    return result;
+}
+
+const file = readFileSync("./day5.txt", "utf-8");
+const lines = file.split("\r\n").filter(x => x);
+
+const seeds: number[] = lines[0].split(" ").slice(1).map(x => parseInt(x));
+
+const maps: SeedMap[] = [];
+
+let mapId = "";
+let ranges: Range[] = [];
+
+for (const line of lines.slice(1)) {
+    if (line[0].match(/[a-z]/)) {
+        if (mapId) {
+            maps.push({ id: mapId, ranges } as SeedMap);
+            mapId = "";
+        }
+
+        mapId = line.split(" ")[0];
+        ranges = [];
+    } else {
+        const [destinationStart, sourceStart, rangeLength] = line.split(" ").map(x => parseInt(x));
+        ranges.push({ destinationStart, sourceStart, rangeLength } as Range);
+    }
+}
+
+maps.push( {id: mapId, ranges } as SeedMap);
+
+let minResult = Infinity;
+
+for (const seed of seeds) {
+    let result = seed;
+
+    for (const map of maps) {
+        result = getMapValue(map.ranges, result);
+    }
+
+    if (result < minResult) {
+        minResult = result;
+    }
+}
+
+console.log(minResult);
